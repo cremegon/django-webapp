0e92c56b0a85267c11da0c2ac5cae97ded9d012e
cremegon <hussainahmed.s.shaikh@gmail.com>
2024-07-29 16:19:10 +0100

    sent: from laptop

--- base/models.py
+++ base/models.py
@@ -11,11 +11,10 @@ class Topic(models.Model):
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    #topic = 
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #participants = 
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -32,5 +31,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["-updated","-created"]
+        
     def __str__(self):
         return self.body[0:50]
