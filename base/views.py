364957402796a18f7299547660319a8ca0d24ec0
cremegon <hussainahmed.s.shaikh@gmail.com>
2024-07-30 13:34:06 +0100

    add: new CSS and view

--- base/views.py
+++ base/views.py
@@ -97,17 +97,32 @@ def room(request, pk):
     context = {"room": room, "room_messages": room_messages, "participants": participants}
     return render(request, "base/room.html",context)
 
+def userProfile(request,pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {"user":user, "rooms": rooms, "room_messages": room_messages, "topics": topics}
+    return render(request, 'base/profile.html', context)
+
 
 @login_required(login_url="/login")
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
 
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    context = {"form":form}
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get("name"),
+            description = request.POST.get("description"),
+        )
+        return redirect("home")
+    context = {"form":form, "topics": topics}
     return render(request, "base/room_form.html", context)
 
 
@@ -115,16 +130,22 @@ def createRoom(request):
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse("You are not verified.")
 
     if request.method == "POST":
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
         form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-    context = {"form":form}
+        room.name = request.POST.get("name")
+        room.topic = topic
+        room.description = request.POST.get("description")
+        room.save()
+
+        return redirect("home")
+    context = {"form":form, "topics": topics, "room":room}
     return render(request, "base/room_form.html", context)
 
 
