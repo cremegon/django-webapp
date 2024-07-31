446fc242eea08329942872b55dfe1bd8da43b0e7
cremegon <hussainahmed.s.shaikh@gmail.com>
2024-07-31 15:17:46 +0100

    add: made final changes in functionality and new user form

--- base/models.py
+++ base/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True,null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
 # Create your models here.
-
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
@@ -35,4 +44,4 @@ class Message(models.Model):
         ordering = ["-updated","-created"]
         
     def __str__(self):
-        return self.body[0:50]
+        return self.body[0:50] 
